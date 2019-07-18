d6864f65443a1a3e8ddf17543be029f046acbe7c
eleboss <you@example.com>
2019-07-18 09:39:14 +0800

    push

--- bag_reader.py
+++ bag_reader.py
@@ -1,21 +1,28 @@
 import subprocess, shlex, rospy, rosbag
 import numpy as np
+import os
+
 
 ev_width = 1280 #x
 ev_height = 800 #y
 
+entries = os.listdir('./')
+for files in entries:
+    if files.endswith(".bag"):
+        file_name = os.path.splitext(files)[0]
+        bag = rosbag.Bag(files)
+        with open(file_name + ".txt", "w") as text_file:
+            for topic, msg, t in bag.read_messages(topics=['/celex/events']):
+                print topic,t
+                # print msg.events[0].polarity
+                
+                for i in msg.events:
+                    ev_x = i.x
+                    ev_y = i.y
+                    ev_p = int(i.polarity == 'True')
+                    ev_t = i.ts.secs + i.ts.nsecs * 1e-6    
+                    text_file.write('%f' % (ev_t) + " " + str(ev_x) + " " + str(ev_y) + " " + str(ev_p) + "\n") 
 
-filename = "luosidao"
-exp_image = np.zeros((ev_height,ev_width))
-bag = rosbag.Bag(filename+".bag")
-for topic, msg, t in bag.read_messages(topics=['/celex/events']):
-    print topic,t
-    # print msg.events[0].polarity
-    for i in msg.events:
-        sum_image[799-i.y ,i.x] = i.ts.secs + i.ts.nsecs * 10e-6
-        
-# cv2.waitKey(1000)
-
-for topic, msg, t in bag.read_messages(topics=['/cam/image_raw']):
-    print topic,t
-bag.close()
+        for topic, msg, t in bag.read_messages(topics=['/cam/image_raw']):
+            print topic,t
+        bag.close()
